f56e829ff84632a19516e1b9d2949b5f05897b43
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-02-13 02:58:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ server.listen(process.env.PORT || 5000, function () {
 
 // Anti-Crash mp
 
-Bah sur ton bot met ca 
 if(message.channel.type === 'dm') return message.reply("Vous ne pouvez pas m'utilisez en message privé.");
 
 // language utiliser par le bot 
